d0f55e94382cef2cacccecf0c6c2a3c654c39ca3
Taisa Braga Bento <taisabraga.adm@gmail.com>
2022-08-04 15:02:36 -0300

    Create Word List

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Maçã', 'Pêra', 'Mamão', 'Laranja'],
+    corpo: ['Braço', 'Perna', 'Pescoço', 'Cabeça', 'Barriga'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete'],
+    programação: ['Linguagem', 'FrameWork', 'JavaScript', 'React'],
+    alimento: ['Arroz', 'Feijao', 'Macarrão', 'Acucar', 'Palmito']
+}
