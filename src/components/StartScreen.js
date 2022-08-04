ec38bc1affb8d7a9c49631848a8358cd994e7c51
Taisa Braga Bento <taisabraga.adm@gmail.com>
2022-08-04 15:18:25 -0300

    Create the StartScreen component and style it

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import './StartScreen.css'
+
+export default function StartScreen(){
+    return(
+        <div className='start'>
+            <h1>Screct Word</h1>
+            <p>Clique no botao abaixo para começar!</p>
+            <button>Começar o Jogo!</button>
+        </div>
+    )
+}
