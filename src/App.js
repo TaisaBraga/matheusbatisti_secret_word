fac60e07e3c3702783a1734ff26890a608bd7bb1
taisa.braga <taisabraga.adm@gmail.com>
2022-08-18 12:12:17 -0300

    Choosing word and category at random

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
-import { useState } from 'react';
-import './App.css';
-import Game from './components/Game';
-import GameOver from './components/GameOver';
-import StartScreen from './components/StartScreen'
-import { wordsList } from './data/words';
+import { useState } from "react";
+import "./App.css";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+import StartScreen from "./components/StartScreen";
+import { wordsList } from "./data/words";
 
 const stages = [
-  {id: 1, name: "start"},
-  {id: 2, name: "game"},
-  {id: 3, name: "end"}
-]
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
 
-  const [gameStage, setGameStage] = useState(stages[0].name)
-  const [words] = useState(wordsList)
+  console.log(words);
 
-  console.log(words)
+  const pickedWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
 
   const startGame = () => {
-    setGameStage(stages[1].name)
-  }
+    const { word, category } = pickedWordAndCategory();
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((letter) => letter.toLowerCase());
+
+    setGameStage(stages[1].name);
+
+  };
 
   const verifyWord = () => {
-    setGameStage(stages[2].name)
-  }
+    setGameStage(stages[2].name);
+  };
 
   const restart = () => {
-    setGameStage(stages[0].name)
-  }
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen startGame={startGame}/>}
-      {gameStage === "game" && <Game verifyWord={verifyWord}/>}
-      {gameStage === "end" && <GameOver restart={restart}/>}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyWord={verifyWord} />}
+      {gameStage === "end" && <GameOver restart={restart} />}
     </div>
   );
 }
